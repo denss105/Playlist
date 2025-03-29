c0200efef3290f739d6e943865d6df15bb200448
EquipoDell <EquipoDell@192.168.0.164>
2025-03-28 23:01:31 -0600

    Se creo este codigo acerca o simulando una playlist con metodos con ayuda de un video de clases.

---
+++ src/main/java/com/mycompany/playlist1/Playlist1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist1;
+
+/**
+ *
+ * @author Denisse Joya
+ */
+public class Playlist1 {
+
+    public static void main(String[] args) {
+       Musicas P1=new Musicas("CORA DE HIELO","Natt Calma","2024",2);
+      P1.ShowPlaylist();
+    }
+}
